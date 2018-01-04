7ed3cee32d3d4c498d65b34a9ef656c1018578f8
Oleksandr Siianko <oleksandr.siyanko@gmail.com>
2018-01-04 14:13:52 +0100

    change to static website

--- app/services/Validator.scala
+++
@@ -1,9 +0,0 @@
-package services
-
-object Validator {
-
-  def isValidUserName(uName: String) =  uName.matches("^[А-Яа-яєЄіІїЇ ']{1,32}$")
-
-  def isValidComment(comment: String) = comment.matches("^[А-Яа-яєЄіІїЇ .,'!?_-]{1,300}$")
-
-}
--- project/plugins.sbt
+++
@@ -1,5 +0,0 @@
-// The Play plugin
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.5.14")
-
-// In-memory DynamoDb plugin
-addSbtPlugin("com.localytics" % "sbt-dynamodb" % "1.5.5")
